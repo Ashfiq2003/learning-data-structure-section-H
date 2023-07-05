ae02eebbdd2b1e2bb43659eb3933d30888b2428a
Ashfiq2003 <121784211+Ashfiq2003@users.noreply.github.com>
2023-07-05 09:13:49 +0600

    Add files via upload

---
+++ 2.Initialize TWO integer arrays A and B of different sizes. Make a new array with the common elements between A and B. Print the new array element(s). If there is no common element, output “No common element!”.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+        {
+int arr1[] ={1,4,6,3,6,9};
+int arr2[] ={5,3,7,1,2,6};
+
+int size1= sizeof(arr1)/ sizeof(arr1[0]);
+int size2= sizeof(arr2)/ sizeof(arr2[0]);
+
+    int celements[min(size1, size2)];
+    int count = 0;
+
+    for (int i = 0; i < size1; i++) {
+        for (int j = 0; j < size2; j++) {
+            if (arr1[i] == arr2[j]) {
+                celements[count++] = arr1[i];
+                break;
+            }
+        }
+    }
+
+    if (count == 0) {
+        cout << "No common Herwe";
+    } else {
+        for (int i = 0; i < count; ++i) {
+            cout << celements[i] << " ,";
+        }
+    }
+
+}
